2446b5a8751a1d49eda37928b98a6c9ccdf3e241
Eva Simon <eva.simon@gmail.com>
2016-05-23 08:21:55 -0500

    change to double quote

--- dist/js/perfmatters.js
+++ dist/js/perfmatters.js
@@ -1,2 +1,2 @@
-/*! web-optimization-project 2016-05-22 */
+/*! web-optimization-project 2016-05-23 */
 function logCRP(){var a=window.performance.timing,b=a.domContentLoadedEventStart-a.domLoading,c=a.domComplete-a.domLoading,d=document.getElementById("crp-stats");d.textContent="DCL: "+b+"ms, onload: "+c+"ms"}window.addEventListener("load",function(a){logCRP()});
